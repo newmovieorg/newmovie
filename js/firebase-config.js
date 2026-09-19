1511f38e3684e1d7c8ec88828cd307bb7a57187a
newmovieorg <code416@gmail.com>
2026-09-19 11:55:55 +0430

    Clear repository

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// تنظیمات پروژه فایربیس شما (new-movie-396bc)
-export const firebaseConfig = {
-  apiKey: "AIzaSyA584r4R-1-r9IwceWzH6cNzN0WOu7DZ9M",
-  authDomain: "new-movie-396bc.firebaseapp.com",
-  projectId: "new-movie-396bc",
-  storageBucket: "new-movie-396bc.firebasestorage.app",
-  messagingSenderId: "599794057590",
-  appId: "1:599794057590:web:8d1a5bee6f8e1bf9507a57"
-};
